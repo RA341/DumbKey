a1fc874ba6b66d4c49745225c6be0c2379e2f0c4
ra341 <razerishi1505@gmail.com>
2023-06-23 15:17:07 +0530

    added encryption code

--- lib/encryptor.dart
+++ lib/encryptor.dart
@@ -1,20 +1,30 @@
+import 'package:dumbkey/constants.dart';
 import 'package:encrypt/encrypt.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class AESEncryption{
-
-  AESEncryption(){
-    final key = Key.fromUtf8('');
-    iv = IV.fromLength(16);
-    encryptionManager = Encrypter(AES(key));
+class AESEncryption {
+  AESEncryption() {
+    final envKey = dotenv.get(
+      Constants.key,
+      fallback: Constants.noKey,
+    );
+    if (envKey == Constants.noKey) throw Exception('No key found');
+    final key = Key.fromUtf8(envKey);
+    _iv = IV.fromLength(16);
+    _encryptionManager = Encrypter(AES(key));
   }
 
-  late final Encrypter encryptionManager;
-  late final IV iv;
+  late final Encrypter _encryptionManager;
+  late final IV _iv;
 
-  static String encrypt(String data){
-    return data;
+  String encrypt(String data) {
+    final en = _encryptionManager.encrypt(data, iv: _iv);
+    return en.base64;
   }
-  static String decrypt(String data){
+
+  String decrypt(String encryptedData) {
+    final encrypted = Encrypted.fromBase64(encryptedData);
+    final data = _encryptionManager.decrypt(encrypted,iv: _iv);
     return data;
   }
 }
