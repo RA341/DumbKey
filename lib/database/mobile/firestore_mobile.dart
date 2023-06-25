c1e4820bce2bf7cc6145586af66fffe29e648416
ra341 <razerishi1505@gmail.com>
2023-06-25 18:49:22 +0530

    separated mobile implementation so that isar can be used

---
+++ lib/database/mobile/firestore_mobile.dart
@@ -0,0 +1,61 @@
+import 'package:dumbkey/database/firestore_stub.dart';
+import 'package:dumbkey/database/mobile/firestore_native.dart';
+import 'package:dumbkey/model/passkey_model.dart';
+import 'package:dumbkey/utils/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:isar/isar.dart';
+
+/// used this way because async constructors are not allowed
+/// we initialize firebase here as it
+/// removes dependency from main helps in tree shaking
+Future<MobileFireStore> initMobileFirestore(Isar isar) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  return MobileFireStore(isar);
+}
+
+class MobileFireStore extends FireStoreBase {
+  MobileFireStore(super.isar) {
+    firestore = FireStoreNative();
+    _listenToFireBaseChanges();
+  }
+
+  late final FireStoreNative firestore;
+
+  @override
+  Future<void> createPassKey(PassKey passkey) async {
+    await firestore.createPassKey(passkey);
+    await isarCreateOrUpdate(passkey);
+  }
+
+  @override
+  Future<void> deletePassKey(PassKey passkey) async {
+    await firestore.deletePassKey(passkey);
+    await isarDelete(passkey.docId); // let the fire store complete first
+  }
+
+  @override
+  Future<void> updatePassKey(String docId, Map<String, dynamic> updateData) async {
+    assert(
+      updateData.containsKey('docId'),
+      'update data does not contain ID,$updateData',
+    );
+    assert(
+      updateData.containsValue(null),
+      'update data contains null,$updateData',
+    );
+
+    await isarCreateOrUpdate(PassKey.fromJson(updateData));
+    await firestore.updatePassKey(docId, updateData);
+  }
+
+  @override
+  Stream<List<PassKey>> fetchAllPassKeys() {
+    return isarDb.passKeys.where().build().watch(fireImmediately: true);
+  }
+
+  void _listenToFireBaseChanges() {
+    firestore.fetchAllPassKeys().listen((passkeys) async {
+      await isarCreateOrUpdateAll(passkeys);
+    });
+  }
+}
--- lib/database/mobile/firestore_native.dart
+++ lib/database/mobile/firestore_native.dart
@@ -1,24 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dumbkey/database/firestore_stub.dart';
 import 'package:dumbkey/logic/encryptor.dart';
 import 'package:dumbkey/model/passkey_model.dart';
 import 'package:dumbkey/utils/constants.dart';
-import 'package:dumbkey/utils/firebase_options.dart';
 import 'package:dumbkey/utils/helper_func.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:isar/isar.dart';
+import 'package:flutter/foundation.dart';
 
-/// used this way because async constructors are not allowed
-/// we initialize firebase here as it
-/// removes dependency from main helps in tree shaking
-Future<MobileFireStore> initMobileFirestore(Isar isar) async {
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  return MobileFireStore(isar);
-}
-
-class MobileFireStore extends FireStoreBase {
-  MobileFireStore(super.isar) {
+class FireStoreNative { // rename this and the file to something else
+  FireStoreNative() {
     database = FirebaseFirestore.instance;
     encryptor = AESEncryption();
   }
@@ -26,15 +14,14 @@ class MobileFireStore extends FireStoreBase {
   late final FirebaseFirestore database;
   late final AESEncryption encryptor;
 
-  @override
   Future<void> createPassKey(PassKey passkey) async {
-    passkey..crypt(encryptor.encrypt)
-    ..docId = idGenerator();
+    passkey
+      ..crypt(encryptor.encrypt)
+      ..docId = idGenerator();
     final doc = database.collection(Constants.mainCollection).doc(passkey.docId.toString());
     await doc.set(passkey.toJSON());
   }
 
-  @override
   Future<void> deletePassKey(PassKey passkey) async {
     try {
       await database.collection(Constants.mainCollection).doc(passkey.docId.toString()).delete();
@@ -43,7 +30,6 @@ class MobileFireStore extends FireStoreBase {
     }
   }
 
-  @override
   Future<void> updatePassKey(String docId, Map<String, dynamic> updateData) async {
     try {
       for (final key in updateData.keys) {
@@ -56,14 +42,11 @@ class MobileFireStore extends FireStoreBase {
     }
   }
 
-  @override
   Stream<List<PassKey>> fetchAllPassKeys() {
     return database.collection(Constants.mainCollection).snapshots().map(
           (snapshots) => snapshots.docs
-              .map(
-                (doc) => PassKey.fromJson(doc.data())..crypt(encryptor.decrypt),
-              )
-              .toList(),
-        );
+          .map((doc) => PassKey.fromJson(doc.data())..crypt(encryptor.decrypt))
+          .toList(),
+    );
   }
 }
