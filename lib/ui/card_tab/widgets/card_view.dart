d39f06893baddc4ee701844c74e68a74a05f70f7
ra341 <razerishi1505@gmail.com>
2023-07-02 22:09:02 +0530

    added shared display tile

--- lib/ui/card_tab/widgets/card_view.dart
+++ lib/ui/card_tab/widgets/card_view.dart
@@ -1,11 +1,8 @@
 // ignore_for_file: inference_failure_on_instance_creation
 
-import 'package:dumbkey/database/database_handler.dart';
 import 'package:dumbkey/model/card_details_model/card_details_model.dart';
-import 'package:dumbkey/ui/card_tab/add_card.dart';
-import 'package:dumbkey/ui/card_tab/card_details_page.dart';
+import 'package:dumbkey/ui/shared/grid_tile.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 
 class CardDetailsView extends StatelessWidget {
   const CardDetailsView({
@@ -22,72 +19,8 @@ class CardDetailsView extends StatelessWidget {
       child: GridView(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
         children: cardsList.map((note) {
-          return CardDetailTile(card: note);
+          return SharedCardTile(data: note);
         }).toList(),
-      ),
-    );
-  }
-}
-
-class CardDetailTile extends StatelessWidget {
-  const CardDetailTile({
-    required this.card,
-    super.key,
-  });
-
-  final CardDetails card;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => CardDetailsPage(card: card)));
-      },
-      onLongPress: () {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => AddCard(savedCard: card)));
-      },
-      child: Stack(
-        alignment: Alignment.center,
-        fit: StackFit.expand,
-        children: [
-          Card(
-            color: Colors.transparent,
-            child: ListTile(title: Text(card.title)),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Align(
-              alignment: Alignment.bottomRight,
-              child: IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () => showDialog<bool>(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text('Are you sure?'),
-                    content: const Text(
-                      'This will permanently delete the Card.',
-                    ),
-                    actions: [
-                      TextButton(
-                        onPressed: () => Navigator.of(context).pop(false),
-                        child: const Text('No'),
-                      ),
-                      TextButton(
-                        onPressed: () async {
-                          await GetIt.I.get<DatabaseHandler>().deleteData(card);
-                          Navigator.of(context).pop(true);
-                        },
-                        child: const Text('Yes'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
--- lib/ui/notes_tab/widgets/notes_list_view.dart
+++ lib/ui/notes_tab/widgets/notes_list_view.dart
@@ -1,9 +1,6 @@
-import 'package:dumbkey/database/database_handler.dart';
 import 'package:dumbkey/model/notes_model/notes_model.dart';
-import 'package:dumbkey/ui/notes_tab/add_notes.dart';
-import 'package:dumbkey/ui/notes_tab/notes_details_page.dart';
+import 'package:dumbkey/ui/shared/grid_tile.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({
@@ -20,71 +17,8 @@ class NotesView extends StatelessWidget {
         crossAxisCount: 3,
         childAspectRatio: 1.5,
       ),
-      children: notesList.map((note) => NoteDetailTile(note: note)).toList(),
+      children: notesList.map((note) => SharedCardTile(data: note)).toList(),
     );
   }
 }
 
-class NoteDetailTile extends StatelessWidget {
-  const NoteDetailTile({
-    required this.note,
-    super.key,
-  });
-
-  final Notes note;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => NotesDetailsPage(note: note)));
-      },
-      onLongPress: () {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => AddNotes(savedNote: note)));
-      },
-      child: Stack(
-        alignment: Alignment.center,
-        fit: StackFit.expand,
-        children: [
-          Card(
-            color: Colors.transparent,
-            child: ListTile(title: Text(note.title)),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Align(
-              alignment: Alignment.bottomRight,
-              child: IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () => showDialog<bool>(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text('Are you sure?'),
-                    content: const Text(
-                      'This will permanently delete the Note.',
-                    ),
-                    actions: [
-                      TextButton(
-                        onPressed: () => Navigator.of(context).pop(false),
-                        child: const Text('No'),
-                      ),
-                      TextButton(
-                        onPressed: () async {
-                          await GetIt.I.get<DatabaseHandler>().deleteData(note);
-                          Navigator.of(context).pop(true);
-                        },
-                        child: const Text('Yes'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
