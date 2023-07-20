37431b056c517d17a5e43b15c3319b4ac2e0a2f0
ra341 <razerishi1505@gmail.com>
2023-07-21 01:53:45 +0530

    clean up

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,38 +1,36 @@
 class DumbData {
-  static const String key = 'ENCRYPTION_KEY';
   static const String noKey = 'no key found';
-  static const String mainCollection = 'main';
-  static const String defaultOrgName = 'any';
 
   static const String firebaseApiKey = 'FIREBASE_APIKEY';
   static const String firebaseProjID = 'FIREBASE_PROJECTID';
 
-  static const String database = 'database';
+  // fields for user
+  static const String salt = 'salt';
+  static const String uuid = 'uuid';
+  static const String userCollection = 'users';
+  static const String encryptionKey = 'encrypt';
 
+  // common fields for all types
   static const String id = 'id';
   static const String dataType = 'dataType';
   static const String title = 'title';
   static const String dateAdded = 'dateAdded';
   static const String syncStatus = 'syncStatus';
+  static const String nonce = 'nonce';
 
+  // fields for passkey
   static const String email = 'email';
   static const String username = 'username';
   static const String password = 'password';
   static const String description = 'description';
   static const String category = 'category';
 
+  // fields for credit card
   static const String cardNumber = 'cardNumber';
   static const String cardHolderName = 'cardHolderName';
   static const String expirationDate = 'expirationDate';
   static const String cvv = 'cvv';
+
+  // fields for notes
   static const String notes = 'notes';
-
-  static const String nonce = 'nonce';
-
-  static const String salt = 'salt';
-  static const String uuid = 'uuid';
-
-  static const String userCollection = 'users';
-
-  static const String encryptionKey = 'encrypt';
 }
